e2c912bc6ce892584c5e56a2cb2487c21e5cb421
Glenn <glenn@not-tech.support>
2023-02-05 15:19:55 +0000

    fix(log): remove main http file log level
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/utils/logger/main.ts
+++ src/utils/logger/main.ts
@@ -4,7 +4,7 @@ import { createLogger, transports } from "winston";
 export const mainLogger = createLogger({
   format: globalFormat,
   transports: [
-    ...createFileTransports("main", ["info", "http"]),
+    ...createFileTransports("main", ["info"]),
     new transports.Console({ level: "info" }),
   ],
 });
